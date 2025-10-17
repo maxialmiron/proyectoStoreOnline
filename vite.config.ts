fff3d2ce0f3c24d45358955a814e64aa9153bbd3
Maximiliano Almiron <77249283+maxialmiron@users.noreply.github.com>
2025-10-17 12:38:16 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  base:'/proyectoStoreOnline/'
+  base:'https://maxialmiron.github.io/proyectoStoreOnline'
 })
